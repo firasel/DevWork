4110301a1c20498dd08e2d89b45002767d812359
firasel <mdrasel601390@gmail.com>
2021-12-04 16:22:27 +0600

    ant design and node sass

---
+++ src/components/Home/Hero/Hero.js
@@ -0,0 +1,13 @@
+import { Typography } from "antd";
+import React from "react";
+
+const Hero = () => {
+  const { Title } = Typography;
+  return (
+    <div>
+      <Title level={2}>This is Hero Section</Title>
+    </div>
+  );
+};
+
+export default Hero;
