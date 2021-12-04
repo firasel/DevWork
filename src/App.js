49493420dc5926d4886ccea882c676e9c9b0f36c
tanzidtahmid <mahamudtanjid @gmail.com>
2021-12-05 00:45:47 +0600

    git problem solved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import Home from "./components/Home/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../Hero/Hero";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
